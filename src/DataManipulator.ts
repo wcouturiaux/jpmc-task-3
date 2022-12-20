bd516220c3e693f847121c276a815583f14be43f
William Couturiaux <37006112+wcouturiaux@users.noreply.github.com>
2022-12-20 00:03:19 -0500

    Add 12mo average for bounds

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -12,13 +12,52 @@ export interface Row {
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+
+  static movingAvg12mo: number = NaN ;
+  static historicRatios: number[] = [];
+
+  static calcMovingAvg12mo(ratio: number): number {
+    let countOfRatios;
+
+    // Account for first value in.
+    if (isNaN(DataManipulator.movingAvg12mo)) {
+        DataManipulator.movingAvg12mo = ratio;
+        DataManipulator.historicRatios.push(ratio)
+        countOfRatios = DataManipulator.historicRatios.length
+        return ratio;
+    }
+    else {
+      countOfRatios = DataManipulator.historicRatios.length
+
+      // Use 252 days as the average number of trading days in a year.
+      if (DataManipulator.historicRatios.length < 252) {
+          DataManipulator.historicRatios.push(ratio);
+          return (DataManipulator.movingAvg12mo*(countOfRatios-1) + ratio)/countOfRatios;
+      }
+      else {
+          let oldValue = DataManipulator.historicRatios.shift();
+          DataManipulator.historicRatios.push(ratio);
+          return (DataManipulator.movingAvg12mo*(countOfRatios-1) - oldValue! + ratio)/countOfRatios
+      }
+    }
+  }
+  static generateRow(serverResponds: ServerRespond[]): Row {
+    const priceABC = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price) / 2;
+    const priceDEF = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price) / 2;
+    const ratio = priceABC/priceDEF;
+    const movingAvg = DataManipulator.calcMovingAvg12mo(ratio);
+    const upperBound = movingAvg*1.05;
+    const lowerBound = movingAvg*0.95;
+
+    return {
+        price_abc: priceABC,
+        price_def: priceDEF,
+        ratio,
+        timestamp: serverResponds[0].timestamp > serverResponds[1].timestamp ?
+            serverResponds[0].timestamp : serverResponds[1].timestamp,
+        upper_bound: upperBound,
+        lower_bound: lowerBound,
+        trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined,
+    };
   }
 }
